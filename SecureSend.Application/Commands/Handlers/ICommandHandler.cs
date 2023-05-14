2e98572cd116b8ae867de442c427a3618ddd8b6c
Radekor500 <radek00500@gmail.com>
2023-05-14 16:09:05 +0200

    command interfaces

---
+++ SecureSend.Application/Commands/Handlers/ICommandHandler.cs
@@ -0,0 +1,14 @@
+﻿
+
+using MediatR;
+
+namespace SecureSend.Application.Commands.Handlers
+{
+    public interface ICommandHandler<TCommand> : IRequestHandler<TCommand> where TCommand: class, ICommand
+    {
+    }
+
+    public interface ICommandHandler<TCommand, TResult> : IRequestHandler<TCommand, TResult> where TCommand : class, ICommand<TResult>
+    {
+    }
+}
---
+++ SecureSend.Application/Commands/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace SecureSend.Application.Commands
+{
+    public interface ICommand : IRequest
+    {
+    }
+
+    public interface ICommand<T> : IRequest<T>
+    {
+    }
+}
