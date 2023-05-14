997343da7feda5a392431abb10be5728edbe923a
Radekor500 <radek00500@gmail.com>
2023-05-14 15:47:31 +0200

    value objects

---
+++ SecureSend.Domain/Repositories/ISecureSendUploadRepository.cs
@@ -0,0 +1,13 @@
+﻿using SecureSend.Domain.Entities;
+using SecureSend.Domain.ValueObjects;
+
+namespace SecureSend.Domain.Repositories
+{
+    public interface ISecureSendUploadRepository
+    {
+        Task<SecureSendUpload> GetAsync(SecureSendUploadId id);
+        Task AddAsync(SecureSendUpload upload);
+        Task DeleteAsync(SecureSendUploadId id);
+        Task UpdateAsync(SecureSendUpload upload);
+    }
+}
