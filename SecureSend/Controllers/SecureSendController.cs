65ecad67b49a63eb3e5bd3d2bed7468becd15729
Radekor500 <radek00500@gmail.com>
2023-05-15 23:43:05 +0200

    basic controller, mediatr test

---
+++ SecureSend/Controllers/SecureSendController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SecureSend.Application.Commands;
+
+namespace SecureSend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SecureSendController: BaseController
+    {
+        protected readonly ISender _sender;
+
+        public SecureSendController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<object>> Post([FromQuery] CreateSecureUpload command)
+        {
+            var result = await _sender.Send(command);
+            return Ok(result);
+        } 
+    }
+}
--- SecureSend/Program.cs
+++ SecureSend/Program.cs
@@ -1,3 +1,6 @@
+using SecureSend.Application;
+using SecureSend.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddSpaStaticFiles(config =>
 {
