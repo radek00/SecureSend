959841f169527e718aa16dfae85f5a55e9a13bff
Radekor500 <radek00500@gmail.com>
2023-05-18 23:17:31 +0200

    CreateSecureUpload command and handler

--- SecureSend.Application/Commands/CreateSecureUpload.cs
+++ SecureSend.Application/Commands/CreateSecureUpload.cs
@@ -1,6 +1,8 @@
 ﻿
 
+using Microsoft.AspNetCore.Http;
+
 namespace SecureSend.Application.Commands
 {
-    public record CreateSecureUpload(Guid uploadId, int chunkNumber, int totalChunks, DateTime expiryDate, string fileName): ICommand<object>;
+    public record CreateSecureUpload(Guid uploadId, int chunkNumber, int totalChunks, DateTime expiryDate, IFormFile chunk): ICommand;
 }
--- SecureSend.Application/Commands/Handlers/CreateSecureUploadHandler.cs
+++ SecureSend.Application/Commands/Handlers/CreateSecureUploadHandler.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using SecureSend.Application.Exceptions;
 using SecureSend.Application.Services;
 using SecureSend.Domain.Factories;
 using SecureSend.Domain.Repositories;
@@ -7,39 +8,40 @@ using SecureSend.Domain.ValueObjects;
 
 namespace SecureSend.Application.Commands.Handlers
 {
-    internal sealed class CreateSecureUploadHandler: ICommandHandler<CreateSecureUpload, object>
+    internal sealed class CreateSecureUploadHandler: ICommandHandler<CreateSecureUpload>
     {
-        //private readonly ISecureSendUploadRepository _secureSendUploadRepository;
-        //private readonly IUploadService _uploadService;
+        private readonly ISecureSendUploadRepository _secureSendUploadRepository;
+        private readonly IFileService _fileService;
         private readonly ISecureSendUploadFactory _secureSendUploadFactory;
 
-        //public CreateSecureUploadHandler(ISecureSendUploadRepository secureSendUploadRepository, IUploadService uploadService, ISecureSendUploadFactory secureSendUploadFactory)
-        //{
-        //    //_secureSendUploadRepository = secureSendUploadRepository;
-        //    //_uploadService = uploadService;
-        //    _secureSendUploadFactory = secureSendUploadFactory;
-        //}
-
-        public CreateSecureUploadHandler(ISecureSendUploadFactory secureSendUploadFactory)
+        public CreateSecureUploadHandler(ISecureSendUploadRepository secureSendUploadRepository,
+                                         IFileService fileService,
+                                         ISecureSendUploadFactory secureSendUploadFactory)
         {
-            //_secureSendUploadRepository = secureSendUploadRepository;
-            //_uploadService = uploadService;
+            _secureSendUploadRepository = secureSendUploadRepository;
+            _fileService = fileService;
             _secureSendUploadFactory = secureSendUploadFactory;
         }
 
-        public async Task<object> Handle (CreateSecureUpload command, CancellationToken cancellationToken)
+        public async Task Handle (CreateSecureUpload command, CancellationToken cancellationToken)
         {
             var secureUpload = _secureSendUploadFactory.CreateSecureSendUpload(command.uploadId, new SecureSendUploadDate(), command.expiryDate, false);
+            var chunk = new SecureUploadChunk(command.chunkNumber, command.totalChunks, command.chunk);        
 
-            //await _uploadService.UploadFileChunk(command.chunkNumber, command.totalChunks, secureUpload.Id);
+            await _fileService.SaveChunkToDisk(chunk, secureUpload.Id);
 
-            if (command.totalChunks == command.chunkNumber)
+            if (chunk.IsLast)
             {
-                secureUpload.AddFile(new SecureSendFile(command.fileName));
-                //await _secureSendUploadRepository.AddAsync(secureUpload);
-            }
+                
+                var savedChunks = _fileService.GetChunksList(secureUpload.Id);
+                if (savedChunks.Count() != chunk.TotalChunks) throw new InvalidChunkCountException(savedChunks.Count(), chunk.TotalChunks);
+                await _fileService.MergeFiles(secureUpload.Id, savedChunks);
 
-            return new();
+                secureUpload.AddFile(new SecureSendFile(chunk.Chunk.FileName, chunk.ContentType));
+                await _secureSendUploadRepository.AddAsync(secureUpload);
+                
+
+            }
         }
 
     }
