e81aa253985e771cf19672a257b58a3f5e966318
Radekor500 <radek00500@gmail.com>
2023-05-18 23:18:07 +0200

    interfaces and exceptions

---
+++ SecureSend.Application/Exceptions/InvalidChunkCountException.cs
@@ -0,0 +1,16 @@
+﻿using SecureSend.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureSend.Application.Exceptions
+{
+    public class InvalidChunkCountException : SecureSendException
+    {
+        public InvalidChunkCountException(int countedChunks, int expectedChunks) : base($"Counted chunks: {countedChunks} are not equal to expected chunks: {expectedChunks}")
+        {
+        }
+    }
+}
---
+++ SecureSend.Application/Services/IFileService.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using SecureSend.Domain.ValueObjects;
+
+namespace SecureSend.Application.Services
+{
+    public interface IFileService
+    {
+        FileStream DownloadFile(Guid uploadId, string fileName);
+        IEnumerable<string> GetChunksList(Guid uploadId);
+        Task MergeFiles(Guid uploadId, IEnumerable<string> chunkFiles);
+        Task SaveChunkToDisk(SecureUploadChunk chunk, Guid uploadId);
+
+    }
+}
