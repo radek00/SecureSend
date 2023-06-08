a9f028601b7c2d2089f89bbf6e5c629f3bb99bdd
Radekor500 <radek00500@gmail.com>
2023-06-08 13:24:28 +0200

    background service for removing failed uploads

--- SecureSend.Infrastructure/BackgroundTasks/BackgroundFailedUploadRemoverService.cs
+++ SecureSend.Infrastructure/BackgroundTasks/BackgroundFailedUploadRemoverService.cs
@@ -1,53 +1,57 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SecureSend.Application.Services;
 using SecureSend.Infrastructure.EF.Context;
+using SecureSend.Infrastructure.EF.Options;
 
 namespace SecureSend.Infrastructure.BackgroundTasks
 {
-    internal sealed class BackgroundFileService: BackgroundService
+    internal sealed class BackgroundFailedUploadRemoverService : BackgroundService
     {
         private readonly ILogger<BackgroundFileService> _logger;
         private readonly IServiceProvider _serviceProvider;
+        private readonly FileStorageOptions _storage;
 
-        public BackgroundFileService(ILogger<BackgroundFileService> logger, IServiceProvider serviceProvider)
+        public BackgroundFailedUploadRemoverService(ILogger<BackgroundFileService> logger, IServiceProvider serviceProvider, IConfiguration configuration)
         {
+            _storage = new();
             _logger = logger;
             _serviceProvider = serviceProvider;
+            configuration.GetSection("FileStoragePath").Bind(_storage);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            await RemoveExpiredUploads(stoppingToken);
+            await RemoveFailedUploads(stoppingToken);
         }
 
-        private async Task RemoveExpiredUploads(CancellationToken token)
+        private async Task RemoveFailedUploads(CancellationToken token)
         {
             while (!token.IsCancellationRequested)
             {
-                _logger.LogInformation("Checking for expired files");
+                _logger.LogInformation("Removing failed uploads");
 
                 using (var scope = _serviceProvider.CreateScope())
                 {
                     var dbContext = scope.ServiceProvider.GetRequiredService<SecureSendDbContext>();
                     var fileService = scope.ServiceProvider.GetRequiredService<IFileService>();
-                    var query = dbContext.SecureSendUploads.Where(u => u.ExpiryDate < DateTime.UtcNow);
-                    var expiredUploads = await query.AsNoTracking().ToListAsync();
-
-                    foreach (var upload in expiredUploads)
+                    var query = dbContext.SecureSendUploads.Where(u => !u.Files.Any());
+                    var emptyUploads = await query.AsNoTracking().ToListAsync();
+                    foreach (var upload in emptyUploads)
                     {
                         fileService.RemoveUpload(upload.Id);
-                        _logger.LogInformation("Removing expired upload: {@id}", upload.Id);
+                        _logger.LogInformation("Removing failed upload: {@id}", upload.Id);
+
                     }
 
-                    if (expiredUploads.Count > 0) await query.ExecuteDeleteAsync();
-
+                    if (emptyUploads.Any()) await query.ExecuteDeleteAsync();
+                    
                     await Task.Delay(TimeSpan.FromMinutes(60), token);
                 }
             }
-
         }
     }
 }
