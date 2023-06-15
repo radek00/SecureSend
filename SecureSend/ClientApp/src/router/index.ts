548ac711a5ee76b918a06a2cc30efde1d21dce10
Radekor500 <radek00500@gmail.com>
2023-06-15 22:25:02 +0200

    multi step form

--- SecureSend/ClientApp/src/router/index.ts
+++ SecureSend/ClientApp/src/router/index.ts
@@ -1,14 +1,14 @@
+import FileUploadView from '@/views/FileUploadView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-//import FileUpload from '../views/FileUpload'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: FileUpload
-    // },
+    {
+      path: '/',
+      name: 'home',
+      component: FileUploadView
+    },
 
   ]
 })
