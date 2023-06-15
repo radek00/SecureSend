70fcc651592bd54a987fe5d2fec7573977fb5ad8
Radekor500 <radek00500@gmail.com>
2023-06-15 22:24:48 +0200

    input components

---
+++ SecureSend/ClientApp/src/components/FileUploadForm/DateInput.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useField } from 'vee-validate';
+
+const props = defineProps<{
+    name:  string;
+}>();
+
+const {errorMessage, value, meta} =  useField(() => props.name)
+</script>
+
+<template>
+    <div>
+        <label :for="props.name"></label>
+        <input :name="props.name" type="date" v-model="value">
+        <p v-if="meta.dirty">{{ errorMessage }}</p>
+    </div>
+</template>
