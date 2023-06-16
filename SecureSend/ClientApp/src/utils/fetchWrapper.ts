fa2bd6df0cfe1494ea7984b71978f198a15d1b3c
Radekor500 <radek00500@gmail.com>
2023-06-16 15:51:07 +0200

    generic types for wrapper

--- SecureSend/ClientApp/src/utils/fetchWrapper.ts
+++ SecureSend/ClientApp/src/utils/fetchWrapper.ts
@@ -5,42 +5,42 @@ export const fetchWrapper = {
     delete: _delete
 };
 
-function get(url: string) {
+function get<T>(url: string): Promise<T> {
     const requestOptions = {
         method: 'GET'
     };
-    return fetch(url, requestOptions).then(handleResponse);
+    return fetch(url, requestOptions).then(handleResponse<T>);
 }
 
-function post(url: string, body?: Body, options?: any) {
+function post<T>(url: string, body?: Body, options?: any): Promise<T> {
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(body)
     };
-    return fetch(url, options ?? requestOptions).then(handleResponse);
+    return fetch(url, options ?? requestOptions).then(handleResponse<T>);
 }
 
-function put(url: string, body?: Body) {
+function put<T>(url: string, body?: Body): Promise<T> {
     const requestOptions = {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(body)
     };
-    return fetch(url, requestOptions).then(handleResponse);    
+    return fetch(url, requestOptions).then(handleResponse<T>);    
 }
 
 // prefixed with underscored because delete is a reserved word in javascript
-function _delete(url: string) {
+function _delete<T>(url: string): Promise<T> {
     const requestOptions = {
         method: 'DELETE'
     };
-    return fetch(url, requestOptions).then(handleResponse);
+    return fetch(url, requestOptions).then(handleResponse<T>);
 }
 
 // helper functions
 
-function handleResponse(response: Response) {
+function handleResponse<T>(response: Response): Promise<T> {
     return response.text().then(text => {
         const data = text && JSON.parse(text);
         
