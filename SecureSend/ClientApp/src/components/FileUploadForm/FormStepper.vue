82b58dc2a38bec2dcec119e781a245f5bcb13b2e
Radekor500 <radek00500@gmail.com>
2023-06-30 16:21:18 +0200

    step icons

--- SecureSend/ClientApp/src/components/FileUploadForm/FormStepper.vue
+++ SecureSend/ClientApp/src/components/FileUploadForm/FormStepper.vue
@@ -1,48 +1,48 @@
 <template>
     <ol class="flex items-center w-full">
-        <li
-            :class="{'text-blue-600 dark:text-blue-500 dark:after:border-blue-800 after:border-blue-100 ': step === 0, 'after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700 ': step !== 0}"
+        <li :class="{ 'text-blue-600 dark:text-blue-500 dark:after:border-blue-800 after:border-blue-100 ': step === 0, 'after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700 ': step !== 0 }"
             class="flex w-full items-center  after:content-[''] after:w-full after:h-1 after:border-b after:border-4 after:inline-block">
-            <span
-                class="flex items-center justify-center w-10 h-10  rounded-full lg:h-12 lg:w-12  shrink-0"
+            <span class="flex items-center justify-center w-10 h-10  rounded-full lg:h-12 lg:w-12  shrink-0"
                 :class="{ 'dark:bg-blue-800 bg-blue-100': step === 0, 'dark:bg-gray-700 bg-gray-100': step !== 0 }">
-                <svg aria-hidden="true" class="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="currentColor"
-                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clip-rule="evenodd"></path>
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor" viewBox="0 0 20 20">
+                    <path
+                        d="M19.728 10.686c-2.38 2.256-6.153 3.381-9.875 3.381-3.722 0-7.4-1.126-9.571-3.371L0 10.437V18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-7.6l-.272.286Z">
+                    </path>
+                    <path
+                        d="m.135 7.847 1.542 1.417c3.6 3.712 12.747 3.7 16.635.01L19.605 7.9A.98.98 0 0 1 20 7.652V6a2 2 0 0 0-2-2h-3V3a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v1H2a2 2 0 0 0-2 2v1.765c.047.024.092.051.135.082ZM10 10.25a1.25 1.25 0 1 1 0-2.5 1.25 1.25 0 0 1 0 2.5ZM7 3a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1H7V3Z">
+                    </path>
                 </svg>
             </span>
         </li>
-        <li
-        :class="{'text-blue-600 dark:text-blue-500 dark:after:border-blue-800 after:border-blue-100 ': step === 1, 'after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700 ': step !== 1}"
+        <li :class="{ 'text-blue-600 dark:text-blue-500 dark:after:border-blue-800 after:border-blue-100 ': step === 1, 'after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700 ': step !== 1 }"
             class="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-4 after:inline-block">
             <span class="flex items-center justify-center w-10 h-10  rounded-full lg:h-12 lg:w-12 shrink-0"
-                  :class="{ 'dark:bg-blue-800 bg-blue-100': step === 1, 'dark:bg-gray-700 bg-gray-100': step !== 1 }">
-                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 lg:w-6 lg:h-6 dark:text-gray-100" fill="currentColor"
-                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                    d="M10 2a1 1 0 00-1 1v1a1 1 0 002 0V3a1 1 0 00-1-1zM4 4h3a3 3 0 006 0h3a2 2 0 012 2v9a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2zm2.5 7a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm2.45 4a2.5 2.5 0 10-4.9 0h4.9zM12 9a1 1 0 100 2h3a1 1 0 100-2h-3zm-1 4a1 1 0 011-1h2a1 1 0 110 2h-2a1 1 0 01-1-1z"
-                    clip-rule="evenodd"></path>
-            </svg>
-        </span>
-    </li>
-    <li class="flex items-center w-full">
-        <span class="flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0"
-              :class="{ 'dark:bg-blue-800 bg-blue-100': step === 2, 'dark:bg-gray-700': step !== 2 }">
-            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 lg:w-6 lg:h-6 dark:text-gray-100" fill="currentColor"
-                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
-                <path fill-rule="evenodd"
-                    d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm9.707 5.707a1 1 0 00-1.414-1.414L9 12.586l-1.293-1.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clip-rule="evenodd"></path>
-            </svg>
-        </span>
-    </li>
-</ol></template>
+                :class="{ 'dark:bg-blue-800 bg-blue-100': step === 1, 'dark:bg-gray-700 bg-gray-100': step !== 1 }">
+
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 20 20">
+                    <path fill="currentColor"
+                        d="M6 1a1 1 0 0 0-2 0h2ZM4 4a1 1 0 0 0 2 0H4Zm7-3a1 1 0 1 0-2 0h2ZM9 4a1 1 0 1 0 2 0H9Zm7-3a1 1 0 1 0-2 0h2Zm-2 3a1 1 0 1 0 2 0h-2ZM1 6a1 1 0 0 0 0 2V6Zm18 2a1 1 0 1 0 0-2v2ZM5 11v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 11v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 15v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 15v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 11v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM5 15v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM2 4h16V2H2v2Zm16 0h2a2 2 0 0 0-2-2v2Zm0 0v14h2V4h-2Zm0 14v2a2 2 0 0 0 2-2h-2Zm0 0H2v2h16v-2ZM2 18H0a2 2 0 0 0 2 2v-2Zm0 0V4H0v14h2ZM2 4V2a2 2 0 0 0-2 2h2Zm2-3v3h2V1H4Zm5 0v3h2V1H9Zm5 0v3h2V1h-2ZM1 8h18V6H1v2Zm3 3v.01h2V11H4Zm1 1.01h.01v-2H5v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H5v2h.01v-2ZM9 11v.01h2V11H9Zm1 1.01h.01v-2H10v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM9 15v.01h2V15H9Zm1 1.01h.01v-2H10v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM14 15v.01h2V15h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM14 11v.01h2V11h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM4 15v.01h2V15H4Zm1 1.01h.01v-2H5v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H5v2h.01v-2Z">
+                    </path>
+                </svg>
+            </span>
+        </li>
+        <li class="flex items-center w-full">
+            <span class="flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0"
+                :class="{ 'dark:bg-blue-800 bg-blue-100': step === 2, 'dark:bg-gray-700': step !== 2 }">
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 16 18">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M8 1v11m0 0 4-4m-4 4L4 8m11 4v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3"></path>
+                </svg>
+            </span>
+        </li>
+    </ol>
+</template>
 
 <script setup lang="ts">
-    const props = defineProps<{
-        step: number;
-    }>();
+const props = defineProps<{
+    step: number;
+}>();
 </script>
