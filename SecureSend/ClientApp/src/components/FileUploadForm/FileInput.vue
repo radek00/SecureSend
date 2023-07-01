e7771e09b557cc5c8ffd727a56f03a257746a333
Radekor500 <radek00500@gmail.com>
2023-07-01 14:40:51 +0200

    extract icons

--- SecureSend/ClientApp/src/components/FileUploadForm/FileInput.vue
+++ SecureSend/ClientApp/src/components/FileUploadForm/FileInput.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { fileSize } from '@/utils/utils'
+import UploadIcon from '@/assets/icons/UploadIcon.vue'
+import FileIcon from '@/assets/icons/FileIcon.vue'
+
 defineEmits(['onFielsChange']);
 
 defineProps<{
@@ -13,7 +16,7 @@ defineProps<{
 <div v-if="!files.size" class="flex items-center justify-center w-full">
     <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
         <div class="flex flex-col items-center justify-center pt-5 pb-6">
-            <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+            <UploadIcon></UploadIcon>
             <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
         </div>
         <input @change="$emit('onFielsChange', $event)" id="dropzone-file" type="file" class="hidden" multiple />
@@ -24,9 +27,7 @@ defineProps<{
     <div v-for="[key, value] in files" class="w-auto p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <div class="flex w-full gap-4 items-center">
             <div>
-                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
-                    <path stroke="currentColor" stroke-linejoin="round" stroke-width="2" d="M6 1v4a1 1 0 0 1-1 1H1m14-4v16a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 1h8.239A.97.97 0 0 1 15 2Z"/>
-                </svg>
+                <FileIcon></FileIcon>
             </div>
             <div class="flex-1 min-w-0">
                 <div class="flex justify-between gap-2">
--- SecureSend/ClientApp/src/components/FileUploadForm/FormStepper.vue
+++ SecureSend/ClientApp/src/components/FileUploadForm/FormStepper.vue
@@ -4,15 +4,7 @@
             class="flex w-full items-center  after:content-[''] after:w-full after:h-1 after:border-b after:border-4 after:inline-block">
             <span class="flex items-center justify-center w-10 h-10  rounded-full lg:h-12 lg:w-12  shrink-0"
                 :class="{ 'dark:bg-blue-800 bg-blue-100': step === 0, 'dark:bg-gray-700 bg-gray-100': step !== 0 }">
-                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor" viewBox="0 0 20 20">
-                    <path
-                        d="M19.728 10.686c-2.38 2.256-6.153 3.381-9.875 3.381-3.722 0-7.4-1.126-9.571-3.371L0 10.437V18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-7.6l-.272.286Z">
-                    </path>
-                    <path
-                        d="m.135 7.847 1.542 1.417c3.6 3.712 12.747 3.7 16.635.01L19.605 7.9A.98.98 0 0 1 20 7.652V6a2 2 0 0 0-2-2h-3V3a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v1H2a2 2 0 0 0-2 2v1.765c.047.024.092.051.135.082ZM10 10.25a1.25 1.25 0 1 1 0-2.5 1.25 1.25 0 0 1 0 2.5ZM7 3a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1H7V3Z">
-                    </path>
-                </svg>
+            <PasswordIcon></PasswordIcon>
             </span>
         </li>
         <li :class="{ 'text-blue-600 dark:text-blue-500 dark:after:border-blue-800 after:border-blue-100 ': step === 1, 'after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700 ': step !== 1 }"
@@ -20,28 +12,22 @@
             <span class="flex items-center justify-center w-10 h-10  rounded-full lg:h-12 lg:w-12 shrink-0"
                 :class="{ 'dark:bg-blue-800 bg-blue-100': step === 1, 'dark:bg-gray-700 bg-gray-100': step !== 1 }">
 
-                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                    fill="none" viewBox="0 0 20 20">
-                    <path fill="currentColor"
-                        d="M6 1a1 1 0 0 0-2 0h2ZM4 4a1 1 0 0 0 2 0H4Zm7-3a1 1 0 1 0-2 0h2ZM9 4a1 1 0 1 0 2 0H9Zm7-3a1 1 0 1 0-2 0h2Zm-2 3a1 1 0 1 0 2 0h-2ZM1 6a1 1 0 0 0 0 2V6Zm18 2a1 1 0 1 0 0-2v2ZM5 11v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 11v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 15v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 15v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 11v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM5 15v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM2 4h16V2H2v2Zm16 0h2a2 2 0 0 0-2-2v2Zm0 0v14h2V4h-2Zm0 14v2a2 2 0 0 0 2-2h-2Zm0 0H2v2h16v-2ZM2 18H0a2 2 0 0 0 2 2v-2Zm0 0V4H0v14h2ZM2 4V2a2 2 0 0 0-2 2h2Zm2-3v3h2V1H4Zm5 0v3h2V1H9Zm5 0v3h2V1h-2ZM1 8h18V6H1v2Zm3 3v.01h2V11H4Zm1 1.01h.01v-2H5v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H5v2h.01v-2ZM9 11v.01h2V11H9Zm1 1.01h.01v-2H10v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM9 15v.01h2V15H9Zm1 1.01h.01v-2H10v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM14 15v.01h2V15h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM14 11v.01h2V11h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM4 15v.01h2V15H4Zm1 1.01h.01v-2H5v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H5v2h.01v-2Z">
-                    </path>
-                </svg>
+                <CalendarIcon></CalendarIcon>
             </span>
         </li>
         <li class="flex items-center">
             <span class="flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0"
                 :class="{ 'dark:bg-blue-800 bg-blue-100': step === 2, 'dark:bg-gray-700': step !== 2 }">
-                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                    fill="none" viewBox="0 0 16 18">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M8 1v11m0 0 4-4m-4 4L4 8m11 4v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3"></path>
-                </svg>
+            <UploadIconStep></UploadIconStep>
             </span>
         </li>
     </ol>
 </template>
 
 <script setup lang="ts">
+import PasswordIcon from '@/assets/icons/PasswordIcon.vue';
+import CalendarIcon from '@/assets/icons/CalendarIcon.vue';
+import UploadIconStep from '@/assets/icons/UploadIconStep.vue';
 const props = defineProps<{
     step: number;
 }>();
