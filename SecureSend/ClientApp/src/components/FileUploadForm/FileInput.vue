e35c0c301e567afd47740fe4b45ebd84afaa420f
Radekor500 <radek00500@gmail.com>
2023-07-02 15:25:31 +0200

    file download styles

--- SecureSend/ClientApp/src/components/FileUploadForm/FileInput.vue
+++ SecureSend/ClientApp/src/components/FileUploadForm/FileInput.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { fileSize } from "@/utils/utils";
 import UploadIcon from "@/assets/icons/UploadIcon.vue";
-import FileIcon from "@/assets/icons/FileIcon.vue";
+import FileCard from "../FileCard.vue";
 
 defineEmits(["onFielsChange"]);
 
@@ -36,32 +35,21 @@ defineProps<{
     v-else
     class="flex flex-col gap-5 mt-5 w-full justify-between max-h-[300px] overflow-y-auto p-6 border border-gray-300 rounded-lg shadow dark:bg-gray-700 dark:border-gray-600"
   >
-    <div
+    <FileCard
       v-for="[key, value] in files"
-      class="w-auto p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+      :file-name="key.name"
+      :size="key.size"
     >
-      <div class="flex w-full gap-4 items-center">
-        <div>
-          <FileIcon></FileIcon>
-        </div>
-        <div class="flex-1 min-w-0">
-          <div class="flex justify-between gap-2">
-            <div class="flex-1 min-w-0">
-              <p class="truncate">{{ key.name }}</p>
-            </div>
-            <p>{{ fileSize(key.size) }}</p>
-          </div>
-
-          <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
-            <div
-              class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
-              :style="{ width: `${value}%` }"
-            >
-              {{ value }}%
-            </div>
+      <template #cardBottom>
+        <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+          <div
+            class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+            :style="{ width: `${value}%` }"
+          >
+            {{ value }}%
           </div>
         </div>
-      </div>
-    </div>
+      </template>
+    </FileCard>
   </div>
 </template>
--- SecureSend/ClientApp/src/views/FileDownloadView.vue
+++ SecureSend/ClientApp/src/views/FileDownloadView.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import StyledButton from "@/components/FileUploadForm/StyledButton.vue";
 import endpoints from "@/config/endpoints";
 import type { SecureUploadDto } from "@/models/SecureUploadDto";
+import { ButtonType } from "@/models/enums/ButtonType";
 import { verifyHash } from "@/utils/pbkdfHash";
 import { ref } from "vue";
+import SimpleInput from "@/components/SimpleInput.vue";
+import { computed } from "vue";
+import FileCard from "@/components/FileCard.vue";
 
 const props = defineProps<{
   secureUpload: SecureUploadDto;
@@ -41,33 +46,76 @@ const verifyPassword = async () => {
   isPasswordValid.value = await verifyHash(props.passwordHash, password.value);
   if (isPasswordValid.value) setUpWorker();
 };
+
+const isPasswordValidComputed = computed(
+  () => isPasswordValid.value === true || isPasswordValid.value === undefined
+);
 </script>
 <template>
-  <h1>Download files</h1>
-
-  <div v-if="isPasswordValid">
-    <div v-for="file in secureUpload.files" :key="(file.fileName as string)">
-      <p>{{ file.fileName }}</p>
-      <a
-        href="#"
-        @click.prevent
-        @click="
-          download(
-            `${endpoints.download}?id=${secureUpload.secureUploadId}&fileName=${file.fileName}`
-          )
-        "
-        >Download</a
+  <div class="w-11/12 md:w-6/12" v-if="isPasswordValid">
+    <h1 class="text-4xl text-gray-900 dark:text-white text-center">
+      Download files
+    </h1>
+    <div
+      class="flex flex-col gap-5 mt-5 w-full justify-between p-6 border border-gray-300 rounded-lg shadow dark:bg-gray-700 dark:border-gray-600"
+    >
+      <FileCard
+        v-for="file in secureUpload.files"
+        :key="(file.fileName as string)"
+        :file-name="file.fileName!"
       >
+        <template #cardBottom>
+          <a
+            class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+            href="#"
+            @click.prevent
+            @click="
+              download(
+                `${endpoints.download}?id=${secureUpload.secureUploadId}&fileName=${file.fileName}`
+              )
+            "
+            >Download</a
+          >
+        </template>
+      </FileCard>
     </div>
   </div>
-  <div v-else>
-    <input
-      v-model="password"
-      name="password"
-      type="password"
-      placeholder="Password"
-    />
-    <p v-show="isPasswordValid === false">Invalid password</p>
-    <button @click="verifyPassword()">Unlock</button>
+
+  <div
+    v-else
+    class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 w-full"
+  >
+    <a
+      href="#"
+      class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+    >
+      SecureSend
+    </a>
+    <div
+      class="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8"
+    >
+      <h2
+        class="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+      >
+        Unlock your files
+      </h2>
+      <div class="mt-4 space-y-4 lg:mt-5 md:space-y-5">
+        <div>
+          <SimpleInput
+            name="password"
+            placeholder="••••••••"
+            label="Decryption password"
+            type="password"
+            v-model="password"
+            :value="password"
+            :isValid="isPasswordValidComputed"
+            errorMessage="Invalid password"
+          ></SimpleInput>
+        </div>
+        <StyledButton @click="verifyPassword()" :type="ButtonType.primary"
+          >Unlock</StyledButton
+        >
+      </div>
+    </div>
   </div>
 </template>
