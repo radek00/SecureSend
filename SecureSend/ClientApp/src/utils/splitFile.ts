2797b1da6dbe322cefc794988dd1bda7160ce373
Radekor500 <radek00500@gmail.com>
2023-07-09 16:35:57 +0200

    prettier

--- SecureSend/ClientApp/src/utils/splitFile.ts
+++ SecureSend/ClientApp/src/utils/splitFile.ts
@@ -3,29 +3,32 @@
 export default async function splitFile(
   file: File,
   chunkSize: number,
-  callback: (chunk: Uint8Array, sequenceNumber: number, totalChunks: number) => void,
-  transformer?: (chunk: Uint8Array, chunkIndex: number) => any,
+  callback: (
+    chunk: Uint8Array,
+    sequenceNumber: number,
+    totalChunks: number
+  ) => void,
+  transformer?: (chunk: Uint8Array, chunkIndex: number) => any
 ) {
   return new Promise((resolve, reject) => {
-
     const fileSize = file.size;
     const totalChunks = Math.ceil(fileSize / chunkSize);
-    console.log(totalChunks)
+    console.log(totalChunks);
     let offset = 0;
-  
+
     const readChunk = () => {
       const r = new FileReader();
       const blob = file.slice(offset, chunkSize + offset);
       r.onload = readEventHandler;
       r.readAsArrayBuffer(blob);
     };
-  
+
     const readEventHandler = async (evt: any) => {
       if (evt.target.error == null) {
-        const sequenceNumber = (offset / chunkSize);
+        const sequenceNumber = offset / chunkSize;
         offset += evt.target.result.byteLength;
         let data = new Uint8Array(evt.target.result);
-  
+
         if (transformer) {
           try {
             data = await transformer(data, sequenceNumber);
@@ -34,24 +37,22 @@ export default async function splitFile(
             reject(error);
           }
         }
-        
-        
       } else {
         // Read error.
         return;
       }
-  
+
       if (offset >= fileSize) {
         // Done reading file.
         resolve(undefined);
         return;
       }
-  
+
       // Off to the next chunk.
       readChunk();
     };
-  
+
     // Let's start reading the first block.
     readChunk();
-  })
+  });
 }
