c7bda99c6ddbd4732f3abc835e2456ee0d1ecb97
Radekor500 <radek00500@gmail.com>
2023-07-23 19:11:22 +0200

    implement dropzone

--- SecureSend/ClientApp/src/components/FileUploadForm/FileInput.vue
+++ SecureSend/ClientApp/src/components/FileUploadForm/FileInput.vue
@@ -3,16 +3,30 @@ import UploadIcon from "@/assets/icons/UploadIcon.vue";
 import FileCard from "../FileCard.vue";
 import LoadingIndicator from "@/components/LoadingIndicator.vue";
 import CheckIcon from "@/assets/icons/CheckIcon.vue";
+import { ref } from "vue";
+import { useDropZone } from "@/utils/composables/useDropZone";
 
-defineEmits(["onFielsChange"]);
+const emit = defineEmits(["onFielsChange"]);
 
 defineProps<{
   files: Map<File, number | string | boolean>;
 }>();
+
+const fileDropZone = ref<HTMLElement>();
+
+const onDrop = (files: File[] | null) => {
+  emit("onFielsChange", files);
+};
+
+const { isOverDropZone } = useDropZone(fileDropZone, {onDrop});
 </script>
 
 <template>
-  <div v-if="!files.size" class="flex items-center justify-center w-full">
+  <div
+    v-if="!files.size"
+    class="flex items-center justify-center w-full"
+    ref="fileDropZone"
+  >
     <label
       for="dropzone-file"
       class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
@@ -24,7 +38,9 @@ defineProps<{
         </p>
       </div>
       <input
-        @change="$emit('onFielsChange', $event)"
+        @change="
+          $emit('onFielsChange', ($event.target as HTMLInputElement).files)
+        "
         id="dropzone-file"
         type="file"
         class="hidden"
