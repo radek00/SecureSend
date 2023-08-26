0105892d06705fd0eeebfaf5745ed89f98d4b9a8
Radekor500 <radek00500@gmail.com>
2023-08-26 18:29:15 +0200

    checkbox input

---
+++ SecureSend/ClientApp/src/components/CheckboxSchemaInput.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useField } from "vee-validate";
+import SimpleCheckboxInput from "@/components/SimpleCheckboxInput.vue";
+
+const props = defineProps<{
+  name: string;
+  checkedValue: unknown;
+}>();
+
+const { errorMessage, value, meta, handleChange } = useField(
+  () => props.name,
+  undefined,
+  {
+    type: "checkbox",
+    checkedValue: props.checkedValue,
+  }
+);
+</script>
+<template>
+  <div class="mb-6">
+    <SimpleCheckboxInput
+      @change="handleChange(checkedValue)"
+      :value="value"
+      v-model="value"
+      :name="name"
+      v-bind="$attrs"
+      :errorMessage="errorMessage"
+      :isValid="meta.valid"
+    >
+    </SimpleCheckboxInput>
+  </div>
+</template>
