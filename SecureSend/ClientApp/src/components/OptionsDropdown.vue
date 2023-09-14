50ef10861d3adb6dc442f92ffd48b88910d4eda9
Radekor500 <radek00500@gmail.com>
2023-09-14 22:02:25 +0200

    dropdown styling

--- SecureSend/ClientApp/src/components/OptionsDropdown.vue
+++ SecureSend/ClientApp/src/components/OptionsDropdown.vue
@@ -3,11 +3,11 @@
     @click="isExpanded = !isExpanded"
     id="dropdownMenuIconHorizontalButton"
     data-dropdown-toggle="dropdownDotsHorizontal"
-    class="relative inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+    class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
     type="button"
   >
     <svg
-      class="w-5 h-5"
+      class="w-5 h-5 relative"
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="currentColor"
@@ -22,7 +22,7 @@
   <div
     v-if="isExpanded"
     id="dropdown"
-    class="z-10 absolute divide-y divide-gray-100 rounded-lg shadow w-auto bg-gray-900 -translate-x-10"
+    class="z-10 absolute divide-y divide-gray-100 rounded-lg shadow w-auto bg-gray-900"
   >
     <ul
       class="py-2 text-sm text-gray-200"
