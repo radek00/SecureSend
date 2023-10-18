a764b64ae7843c9e8c95f0146f17eb9b31335ef7
Radekor500 <radek00500@gmail.com>
2023-10-18 22:30:28 +0200

    rename button props

--- SecureSend/ClientApp/src/components/StyledButton.vue
+++ SecureSend/ClientApp/src/components/StyledButton.vue
@@ -1,14 +1,13 @@
 <template>
   <button
-    type="submit"
     class="text-white focus:ring-4 focus:enabled::outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:cursor-not-allowed disabled:bg-gray-700"
     :class="{
       'bg-blue-600 hover:enabled::bg-blue-700 focus:enabled:ring-blue-800 hover:enabled:bg-blue-800':
-        type === ButtonType.primary,
+        category === ButtonType.primary,
       'bg-gray-800 hover:enabled:bg-gray-900 focus:enabled:ring-gray-700':
-        type === ButtonType.back,
+        category === ButtonType.back,
       'bg-red-600 hover:bg-red-700 focus:ring-red-900':
-        type === ButtonType.cancel,
+        category === ButtonType.cancel,
     }"
   >
     <slot>Submit</slot>
@@ -19,6 +18,6 @@
 import { ButtonType } from "@/models/enums/ButtonType";
 
 defineProps<{
-  type: ButtonType;
+  category: ButtonType;
 }>();
 </script>
