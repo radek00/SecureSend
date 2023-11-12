deada857a79edd508f5a1bdd61b217da6301a1b1
Radekor500 <radek00500@gmail.com>
2023-11-12 13:51:23 +0100

    improve semantics

--- SecureSend/ClientApp/src/components/FileUploadForm/FileInput.vue
+++ SecureSend/ClientApp/src/components/FileUploadForm/FileInput.vue
@@ -48,15 +48,15 @@ const { isOverDropZone } = useDropZone(fileDropZone, { onDrop });
       for="dropzone-file"
       class="flex flex-col items-center justify-center w-full h-64 border-2 border-dashed rounded-lg cursor-pointer hover:bg-bray-800 bg-gray-700 border-gray-600 hover:border-gray-500 hover:bg-gray-600"
     >
-      <div class="flex flex-col items-center justify-center pt-5 pb-6">
+      <span class="flex flex-col items-center justify-center pt-5 pb-6">
         <UploadIcon></UploadIcon>
-        <p v-if="!isOverDropZone" class="mb-2 text-sm text-gray-400">
+        <span v-if="!isOverDropZone" class="mb-2 text-sm text-gray-400">
           <span class="font-semibold">Click to upload</span> or drag and drop
-        </p>
-        <p v-else class="mb-2 text-sm text-gray-400">
+        </span>
+        <span v-else class="mb-2 text-sm text-gray-400">
           <span class="font-semibold">Drop files to upload</span>
-        </p>
-      </div>
+        </span>
+      </span>
       <input
         @change="
           $emit('onFielsChange', ($event.target as HTMLInputElement).files)
@@ -84,7 +84,7 @@ const { isOverDropZone } = useDropZone(fileDropZone, { onDrop });
             v-if="!isLoading && value !== true"
             @click="emit('onFileRemove', key)"
             type="button"
-            class="hidden md:block m-0 border hover:enabled:bg-red-700 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 border-red-500 hover:enabled:text-white focus:ring-red-800 hover:bg-red-500 disabled:cursor-not-allowed disabled:bg-gray-600 disabled:border-gray-800"
+            class="hidden md:inline-flex m-0 border hover:enabled:bg-red-700 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm p-2.5 text-center items-center mr-2 border-red-500 hover:enabled:text-white focus:ring-red-800 hover:bg-red-500 disabled:cursor-not-allowed disabled:bg-gray-600 disabled:border-gray-800"
           >
             <TrashIcon class="w-5 h-3"></TrashIcon>
             <span class="sr-only">Remove file</span>
