b66bb7e2bb74ed543f9611af3a13f47c87d3509a
Radekor500 <radek00500@gmail.com>
2023-11-12 13:35:07 +0100

    click outside directive

---
+++ SecureSend/ClientApp/src/utils/composables/directives/clickOutside.ts
@@ -0,0 +1,17 @@
+import type { Directive } from "vue";
+
+export const clickOutside: Directive = {
+  beforeMount: function (element, binding) {
+    element.clickOutsideEvent = function (event) {
+      if (
+        !(element === event.target || event.composedPath().includes(element))
+      ) {
+        binding.value(event);
+      }
+    };
+    document.body.addEventListener("click", element.clickOutsideEvent);
+  },
+  unmounted: function (element) {
+    document.body.removeEventListener("click", element.clickOutsideEvent);
+  },
+};
