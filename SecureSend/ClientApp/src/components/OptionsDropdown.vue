33faf801ca032eab21e6db6af75b2f84547cf063
Radekor500 <radek00500@gmail.com>
2023-11-12 13:35:58 +0100

    relative root element for dropdown and directive registration

--- SecureSend/ClientApp/src/components/OptionsDropdown.vue
+++ SecureSend/ClientApp/src/components/OptionsDropdown.vue
@@ -1,22 +1,24 @@
 <template>
-  <button
-    @click="isExpanded = !isExpanded"
-    class="inline-flex items-center p-2 text-sm font-medium text-center rounded-lg focus:ring-4 focus:outline-none text-white focus:ring-gray-50 bg-gray-800 hover:bg-gray-700"
-    type="button"
-  >
-    <ExpandIcon></ExpandIcon>
-  </button>
-  <div
-    v-if="isExpanded"
-    id="dropdown"
-    class="z-10 absolute divide-y divide-gray-100 rounded-lg shadow w-auto bg-gray-900"
-  >
-    <ul
-      class="py-2 text-sm text-gray-200"
-      aria-labelledby="dropdownDefaultButton"
+  <div class="relative" v-click-outside="() => (isExpanded = false)">
+    <button
+      @click="isExpanded = !isExpanded"
+      class="inline-flex items-center p-2 text-sm font-medium text-center rounded-lg focus:ring-4 focus:outline-none text-white focus:ring-gray-50 bg-gray-800 hover:bg-gray-700"
+      type="button"
     >
-      <slot></slot>
-    </ul>
+      <ExpandIcon></ExpandIcon>
+    </button>
+    <div
+      v-if="isExpanded"
+      id="dropdown"
+      class="absolute z-10 divide-y divide-gray-100 rounded-lg shadow w-auto bg-gray-900"
+    >
+      <ul
+        class="py-2 text-sm text-gray-200"
+        aria-labelledby="dropdownDefaultButton"
+      >
+        <slot></slot>
+      </ul>
+    </div>
   </div>
 </template>
 
