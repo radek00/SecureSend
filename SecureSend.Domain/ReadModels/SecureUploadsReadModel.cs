6f8d8bdcbe8056760952da325ba4d73080ec27eb
Radekor500 <radek00500@gmail.com>
2023-11-18 16:33:38 +0100

    remove isProtected from read model

--- SecureSend.Domain/ReadModels/SecureUploadsReadModel.cs
+++ SecureSend.Domain/ReadModels/SecureUploadsReadModel.cs
@@ -6,7 +6,6 @@
         public DateTime? ExpiryDate { get; set; }
         public bool IsViewed { get; set; }
         public DateTime UploadDate { get; set; }
-        public bool IsProtected { get; set; }
         public byte[]? PasswordHash { get; set; }
         public ICollection<UploadedFilesReadModel> Files { get; set; }
     }
