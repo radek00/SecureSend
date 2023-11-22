d861fffe3ed98af41f849f2648fec3d40d8948c6
Radekor500 <radek00500@gmail.com>
2023-11-22 23:19:14 +0100

    prettier

--- SecureSend/ClientApp/src/models/enums/ErrorTypes.ts
+++ SecureSend/ClientApp/src/models/enums/ErrorTypes.ts
@@ -1,5 +1,5 @@
 export enum ErrorTypes {
   upload_does_not_exist = "upload_does_not_exist",
   upload_expired = "upload_expired",
-  invalid_password = "invalid_password"
+  invalid_password = "invalid_password",
 }
