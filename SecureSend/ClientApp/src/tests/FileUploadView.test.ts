7d89ae4fbb2beaceb958b6c7b4f8382df0924d30
Radekor500 <radek00500@gmail.com>
2023-12-27 18:29:12 +0100

    form flow and validation tests

--- SecureSend/ClientApp/src/tests/FileUploadView.test.ts
+++ SecureSend/ClientApp/src/tests/FileUploadView.test.ts
@@ -2,6 +2,7 @@ import { VueWrapper, mount, flushPromises } from "@vue/test-utils";
 import { beforeEach, describe, expect, test, vi } from "vitest";
 import FileUploadView from "@/views/FileUploadView.vue";
 import { ref } from "vue";
+import { waitForExpect } from "@/tests/utils";
 
 describe("FileUploadView", () => {
   let wrapper: VueWrapper<any>;
@@ -15,8 +16,8 @@ describe("FileUploadView", () => {
     await flushPromises();
   });
 
-  test("No password form flow", async () => {
-    let submitButton = wrapper.find('button[type="submit"]');
+  test("Multistep form flow", async () => {
+    const submitButton = wrapper.find('button[type="submit"]');
     console.log(submitButton.text());
     const passwordRequired = wrapper.find('input[type="password"]');
 
@@ -33,12 +34,53 @@ describe("FileUploadView", () => {
     expect(submit).toHaveBeenCalledTimes(2);
 
     await flushPromises();
-    setTimeout(() => {
+    await waitForExpect(() => {
+      console.log(wrapper.vm.currentSchema);
       expect(submitButton.text()).toEqual("Upload");
-    }, 1000);
+      expect(submitButton.attributes("disabled")).toBeDefined();
+    });
+  });
+
+  test("Password validation", async () => {
+    const passwordInput = wrapper.find('input[type="password"]');
+    expect(passwordInput.attributes("disabled")).toBeDefined();
+
+    const requiredCheckbox = wrapper.find('input[name="isPasswordRequired"');
+    expect(requiredCheckbox.attributes("value")).toEqual("false");
+
+    await requiredCheckbox.setValue();
+
+    await waitForExpect(() => {
+      expect(passwordInput.attributes("disabled")).toBeUndefined();
+    });
+
+    await passwordInput.setValue("test");
+    console.log(passwordInput.attributes());
+    await passwordInput.setValue("");
+
+    await waitForExpect(() => {
+      expect(
+        wrapper.find('div[label="Encryption password"]').find("span").text()
+      ).toEqual("Password is required.");
+    });
+  });
+
+  test("Date validation", async () => {
+    const submitButton = wrapper.find('button[type="submit"]');
 
     await submitButton.trigger("submit");
-    await flushPromises();
-    //setTimeout(())
+
+    const dateInput = wrapper.find('input[type="date"]');
+    await dateInput.setValue("2022-12-01");
+
+    await waitForExpect(() => {
+      console.log(dateInput.attributes());
+      console.log(
+        wrapper.find('div[label="Optional expiry date"]').find("span").text()
+      );
+      expect(
+        wrapper.find('div[label="Optional expiry date"]').find("span").text()
+      ).toEqual("Expiry date must be earlier than today.");
+    }, 1000);
   });
 });
