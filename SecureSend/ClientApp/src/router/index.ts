1ea7cbf0f01f45e8e33cb0bc9dc5333a2b4b19d7
Radekor500 <radek00500@gmail.com>
2023-12-30 18:20:31 +0100

    router instance for tests

--- SecureSend/ClientApp/src/router/index.ts
+++ SecureSend/ClientApp/src/router/index.ts
@@ -8,51 +8,53 @@ const FileDownloadView = () => import("@/views/FileDownloadView.vue");
 const ErrorPageView = () => import("@/views/ErrorPageView.vue");
 const LandingView = () => import("@/views/LandingView.vue");
 
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: LandingView,
+  },
+  {
+    path: "/upload",
+    name: "upload",
+    component: FileUploadView,
+  },
+  {
+    path: "/error/:errorCode",
+    name: "error",
+    props: true,
+    component: ErrorPageView,
+  },
+  {
+    path: "/download/:id",
+    name: "fileDownload",
+    component: FileDownloadView,
+    props: true,
+    beforeEnter: async (to) => {
+      try {
+        (to.params.verifyUploadResponse as unknown as UploadVerifyResponseDTO) =
+          await SecureSendService.verifySecureUpload(to.params.id as string);
+        const keys = to.hash.split("_");
+        to.params.b64Key = keys[0].slice(1);
+      } catch (error) {
+        if (error instanceof UploadExpiredError) {
+          return {
+            name: "error",
+            params: {
+              errorCode: "410",
+            },
+          };
+        }
+        return { name: "error", params: { errorCode: "404" } };
+      }
+    },
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: LandingView,
-    },
-    {
-      path: "/upload",
-      name: "upload",
-      component: FileUploadView,
-    },
-    {
-      path: "/error/:errorCode",
-      name: "error",
-      props: true,
-      component: ErrorPageView,
-    },
-    {
-      path: "/download/:id",
-      name: "fileDownload",
-      component: FileDownloadView,
-      props: true,
-      beforeEnter: async (to) => {
-        try {
-          (to.params
-            .verifyUploadResponse as unknown as UploadVerifyResponseDTO) =
-            await SecureSendService.verifySecureUpload(to.params.id as string);
-          const keys = to.hash.split("_");
-          to.params.b64Key = keys[0].slice(1);
-        } catch (error) {
-          if (error instanceof UploadExpiredError) {
-            return {
-              name: "error",
-              params: {
-                errorCode: "410",
-              },
-            };
-          }
-          return { name: "error", params: { errorCode: "404" } };
-        }
-      },
-    },
-  ],
+  routes: routes,
 });
 
+export { routes };
 export default router;
