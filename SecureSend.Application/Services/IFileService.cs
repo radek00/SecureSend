4d9a183270c735e22959294deb0d444192136f1f
Radekor500 <radek00500@gmail.com>
2024-01-15 23:18:28 +0100

    handle random file names

--- SecureSend.Application/Services/IFileService.cs
+++ SecureSend.Application/Services/IFileService.cs
@@ -8,7 +8,7 @@ namespace SecureSend.Application.Services
     {
         FileStream DownloadFile(Guid uploadId, string fileName);
         IEnumerable<string> GetChunksList(Guid uploadId, string chunkDirectory);
-        Task MergeFiles(Guid uploadId, IEnumerable<string> chunkFiles, string chunkDirectory);
+        Task MergeFiles(Guid uploadId, IEnumerable<string> chunkFiles, string chunkDirectory, string randomFileName);
         Task SaveChunkToDisk(SecureUploadChunk chunk, Guid uploadId);
 
         void RemoveUpload(Guid uploadId);
