8c536611bb6e2ac726032e91e56dd98f7e22192d
Radekor500 <radek00500@gmail.com>
2024-01-15 23:20:39 +0100

    generate migrations

---
+++ SecureSend.PostgresMigrations/Migrations/20240115212106_RandomFileName.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SecureSend.PostgresMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class RandomFileName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FileName",
+                schema: "upload",
+                table: "UploadedFiles",
+                newName: "RandomFileName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayFileName",
+                schema: "upload",
+                table: "UploadedFiles",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayFileName",
+                schema: "upload",
+                table: "UploadedFiles");
+
+            migrationBuilder.RenameColumn(
+                name: "RandomFileName",
+                schema: "upload",
+                table: "UploadedFiles",
+                newName: "FileName");
+        }
+    }
+}
