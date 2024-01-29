c9c539461ea09b9ceeb5ab59f155bac5aaa96271
Radekor500 <radek00500@gmail.com>
2024-01-30 00:32:12 +0100

    get initial upload directory size at start

--- SecureSend.Infrastructure/HostedServices/AppInitializer.cs
+++ SecureSend.Infrastructure/HostedServices/AppInitializer.cs
@@ -1,32 +1,47 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SecureSend.Application.Services;
+
+namespace SecureSend.Infrastructure.HostedServices;
 
 internal sealed class AppInitializer : IHostedService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public AppInitializer(IServiceProvider serviceProvider)
+        => _serviceProvider = serviceProvider;
+
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
-        private readonly IServiceProvider _serviceProvider;
 
-        public AppInitializer(IServiceProvider serviceProvider)
-            => _serviceProvider = serviceProvider;
-
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            var dbContextTypes = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(a => a.GetTypes())
-                .Where(a => typeof(DbContext).IsAssignableFrom(a) && !a.IsInterface && a != typeof(DbContext));
-
-            using var scope = _serviceProvider.CreateScope();
-            foreach (var dbContextType in dbContextTypes)
-            {
-                var dbContext = scope.ServiceProvider.GetRequiredService(dbContextType) as DbContext;
-                if (dbContext is null)
-                {
-                    continue;
-                }
-
-                await dbContext.Database.MigrateAsync(cancellationToken);
-            }
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+        using var scope = _serviceProvider.CreateScope();
+        await MigrateAsync(scope, cancellationToken);
+        SetupCurrentUploadDirectorySize(scope, cancellationToken);
     }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+    private async Task MigrateAsync(IServiceScope scope, CancellationToken cancellationToken)
+    {
+        var dbContextTypes = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(a => a.GetTypes())
+            .Where(a => typeof(DbContext).IsAssignableFrom(a) && !a.IsInterface && a != typeof(DbContext));
+        foreach (var dbContextType in dbContextTypes)
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService(dbContextType) as DbContext;
+            if (dbContext is null)
+            {
+                continue;
+            }
+
+            await dbContext.Database.MigrateAsync(cancellationToken);
+        }
+    }
+
+    private void SetupCurrentUploadDirectorySize(IServiceScope scope, CancellationToken cancellationToken)
+    {
+        var sizeTrackerService = scope.ServiceProvider.GetRequiredService<IUploadSizeTrackerService>();
+        sizeTrackerService.Setup();
+    }
+}
