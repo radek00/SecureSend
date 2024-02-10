f7189e0441b2924ad7df36f54e3e0b26e4ab0d5e
Radekor500 <radek00500@gmail.com>
2024-02-10 23:31:56 +0100

    resolve warnings

--- SecureSend.Infrastructure/EF/Options/PostgresOptions.cs
+++ SecureSend.Infrastructure/EF/Options/PostgresOptions.cs
@@ -2,9 +2,9 @@ namespace SecureSend.Infrastructure.EF.Options;
 
 public class PostgresOptions
 {
-    public string Host { get; set; }
-    public string Port { get; set; }
-    public string Database { get; set; }
-    public string UserId { get; set; }
-    public string Password{ get; set; }
+    public string? Host { get; set; }
+    public string? Port { get; set; }
+    public string? Database { get; set; }
+    public string? UserId { get; set; }
+    public string? Password{ get; set; }
 }
