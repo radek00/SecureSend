ab4928fe0e7c7fc08d2a0ead065624eef02ce09c
Radekor500 <radek00500@gmail.com>
2024-02-11 13:31:21 +0100

    minor styling fixes

--- SecureSend/ClientApp/src/components/ErrorMessage.vue
+++ SecureSend/ClientApp/src/components/ErrorMessage.vue
@@ -1,7 +1,9 @@
 <template>
-  <p class="mt-2 text-sm text-red-500 absolute">
-    <slot>
-      <span class="font-medium">Oh, snapp!</span> Something went wrong.
-    </slot>
-  </p>
+  <div class="relative">
+    <p class="absolute right-0 mt-2 text-sm text-red-500">
+      <slot>
+        <span class="font-medium">Oh, snapp!</span> Something went wrong.
+      </slot>
+    </p>
+  </div>
 </template>
