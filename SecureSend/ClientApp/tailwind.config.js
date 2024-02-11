2095725a76e1c602dfba29aa461ec7dbed76a738
Radekor500 <radek00500@gmail.com>
2024-02-11 13:03:10 +0100

    change font to inter

--- SecureSend/ClientApp/tailwind.config.js
+++ SecureSend/ClientApp/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
   },
   plugins: [],
 };
