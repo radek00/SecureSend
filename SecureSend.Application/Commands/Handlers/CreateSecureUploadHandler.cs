aaee7a4f246c7c1cba6e227050bf61c206c75632
Radekor500 <radek00500@gmail.com>
2024-02-12 23:36:46 +0100

    ability to set max expirations for uploads

--- SecureSend.Application/Commands/Handlers/CreateSecureUploadHandler.cs
+++ SecureSend.Application/Commands/Handlers/CreateSecureUploadHandler.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Options;
 using SecureSend.Application.Exceptions;
+using SecureSend.Application.Options;
 using SecureSend.Application.Services;
 using SecureSend.Domain.Factories;
 using SecureSend.Domain.Repositories;
@@ -12,21 +14,31 @@ namespace SecureSend.Application.Commands.Handlers
         private readonly ISecureSendUploadRepository _secureSendUploadRepository;
         private readonly ISecureSendUploadFactory _secureSendUploadFactory;
         private readonly ISecureUploadReadService _secureUploadReadService;
+        private readonly IOptions<FileStorageOptions> _fileStorageOptions;
 
         public CreateSecureUploadHandler(ISecureSendUploadRepository secureSendUploadRepository,
                                          ISecureSendUploadFactory secureSendUploadFactory,
-                                         ISecureUploadReadService secureUploadReadService)
+                                         ISecureUploadReadService secureUploadReadService, IOptions<FileStorageOptions> fileStorageOptions)
         {
             _secureSendUploadRepository = secureSendUploadRepository;
             _secureSendUploadFactory = secureSendUploadFactory;
             _secureUploadReadService = secureUploadReadService;
+            _fileStorageOptions = fileStorageOptions;
         }
 
         public async Task<Unit> Handle (CreateSecureUpload command, CancellationToken cancellationToken)
         {
+            var expiryDate = command.expiryDate;
+            if (_fileStorageOptions.Value.MaxExpirationInDays != 0 &&
+                expiryDate > DateTime.UtcNow.AddDays(_fileStorageOptions.Value.MaxExpirationInDays))
+                throw new MaxExpirationExceededException(_fileStorageOptions.Value.MaxExpirationInDays);
+            if (_fileStorageOptions.Value.MaxExpirationInDays != 0 && expiryDate == null)
+            {
+                expiryDate = DateTime.UtcNow.AddDays(_fileStorageOptions.Value.MaxExpirationInDays);
+            }
             var persisted = await _secureUploadReadService.GetUploadId(command.uploadId, cancellationToken);
             if (persisted is not null && persisted != Guid.Empty) throw new UploadAlreadyExistsException(persisted.Value);
-            var secureUpload = _secureSendUploadFactory.CreateSecureSendUpload(command.uploadId, new SecureSendUploadDate(), command.expiryDate, false, command.password);
+            var secureUpload = _secureSendUploadFactory.CreateSecureSendUpload(command.uploadId, new SecureSendUploadDate(), expiryDate, false, command.password);
             await _secureSendUploadRepository.AddAsync(secureUpload, cancellationToken);
             return Unit.Value;
 
