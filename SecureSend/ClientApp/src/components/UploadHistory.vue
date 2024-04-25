c18cbf0485f17b55484bcbf904e607b33ed75ec0
Radekor500 <radek00500@gmail.com>
2024-04-25 21:59:11 +0200

    extract icons to separate components

--- SecureSend/ClientApp/src/components/UploadHistory.vue
+++ SecureSend/ClientApp/src/components/UploadHistory.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import DropdownIcon from "@/assets/icons/DropdownIcon.vue";
+import CopyIcon from "@/assets/icons/CopyIcon.vue";
 </script>
 
 <template>
@@ -13,21 +15,7 @@
             <a
               class="text-sm underline cursor-pointer text-blue-500 hover:no-underline flex items-center gap-2 mt-2"
             >
-              <svg
-                class="w-2.5 h-2.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 10 6"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m1 1 4 4 4-4"
-                />
-              </svg>
+              <DropdownIcon></DropdownIcon>
             </a>
           </div>
           <div class="flex-1 min-w-0 ms-4">
@@ -41,17 +29,7 @@
               class="text-gray-400 bg-gray-800 border-gray-600 hover:bg-gray-700 rounded-lg py-2 px-2.5 inline-flex items-center justify-center border"
             >
               <span id="default-message" class="inline-flex items-center">
-                <svg
-                  class="w-3 h-3 me-1.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 18 20"
-                >
-                  <path
-                    d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z"
-                  />
-                </svg>
+                <CopyIcon></CopyIcon>
                 <span class="text-xs font-semibold">Copy</span>
               </span>
             </button>
