86667aaba9e400dcb93176f1d72503dad6d9443e
Radekor500 <radek00500@gmail.com>
2024-04-26 14:35:05 +0200

    toggle expanding files

---
+++ SecureSend/ClientApp/src/components/UploadHistory/UploadHistory.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import DropdownIcon from "@/assets/icons/DropdownIcon.vue";
+import CopyIcon from "@/assets/icons/CopyIcon.vue";
+import FileCard from "@/components/FileCard.vue";
+import HistoryElement from "@/components/UploadHistory/HistoryElement.vue";
+</script>
+
+<template>
+  <div class="flex items-center justify-between mb-4">
+    <h5 class="text-xl font-bold leading-none text-white">Upload history</h5>
+  </div>
+  <div class="overflow-auto max-h-40 lg:max-h-96 p-2">
+    <ul role="list" class="divide-y divide-gray-700">
+      <HistoryElement upload="{}"></HistoryElement>
+      <HistoryElement upload="{}"></HistoryElement>
+      <HistoryElement upload="{}"></HistoryElement>
+      <HistoryElement upload="{}"></HistoryElement>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
