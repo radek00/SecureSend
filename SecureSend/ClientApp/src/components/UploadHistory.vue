82204fc7784e648c9320d9c15066a2cb639184b9
Radekor500 <radek00500@gmail.com>
2024-04-26 13:58:50 +0200

    history expandable list design

--- SecureSend/ClientApp/src/components/UploadHistory.vue
+++ SecureSend/ClientApp/src/components/UploadHistory.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import DropdownIcon from "@/assets/icons/DropdownIcon.vue";
 import CopyIcon from "@/assets/icons/CopyIcon.vue";
+import FileCard from "@/components/FileCard.vue";
 </script>
 
 <template>
@@ -35,57 +36,29 @@ import CopyIcon from "@/assets/icons/CopyIcon.vue";
             </button>
           </div>
         </div>
-      </li>
-      <li class="py-3 sm:py-4">
-        <div class="flex items-center">
-          <div class="flex-shrink-0">
-            <a
-              class="text-sm underline cursor-pointer text-blue-500 hover:no-underline flex items-center gap-2 mt-2"
-            >
-              <svg
-                class="w-2.5 h-2.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 10 6"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m1 1 4 4 4-4"
-                />
-              </svg>
-            </a>
-          </div>
-          <div class="flex-1 min-w-0 ms-4">
-            <p class="text-sm font-medium truncate text-white">23.04.2024</p>
-            <p class="text-sm truncate text-gray-400">5 files</p>
-          </div>
-          <div
-            class="inline-flex items-center text-base font-semibold text-white"
-          >
-            <button
-              class="text-gray-400 bg-gray-800 border-gray-600 hover:bg-gray-700 rounded-lg py-2 px-2.5 inline-flex items-center justify-center border"
-            >
-              <span id="default-message" class="inline-flex items-center">
-                <svg
-                  class="w-3 h-3 me-1.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 18 20"
-                >
-                  <path
-                    d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z"
-                  />
-                </svg>
-                <span class="text-xs font-semibold">Copy</span>
-              </span>
-            </button>
-          </div>
-        </div>
+        <ul class="ml-[1.6rem] mt-3 flex flex-col gap-3">
+          <li>
+            <FileCard
+              file-name="hello.exe"
+              :size="5000"
+              class="bg-slate-700"
+            ></FileCard>
+          </li>
+          <li>
+            <FileCard
+              file-name="hello.exe"
+              :size="5000"
+              class="bg-slate-700"
+            ></FileCard>
+          </li>
+          <li>
+            <FileCard
+              file-name="hello.exe"
+              :size="5000"
+              class="bg-slate-700"
+            ></FileCard>
+          </li>
+        </ul>
       </li>
     </ul>
   </div>
