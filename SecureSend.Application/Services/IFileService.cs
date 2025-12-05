452660a1d76d9226fe53057701dbb405bb13b1ee
radek00 <radekw00@proton.me>
2025-12-05 16:45:51 +0100

    merge upload chunks as they come

--- SecureSend.Application/Services/IFileService.cs
+++ SecureSend.Application/Services/IFileService.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using SecureSend.Domain.ValueObjects;
 
 namespace SecureSend.Application.Services
@@ -9,6 +8,8 @@ namespace SecureSend.Application.Services
         FileStream? DownloadFile(Guid uploadId, string fileName);
         IEnumerable<string> GetChunksList(Guid uploadId, string chunkDirectory);
         Task MergeFiles(Guid uploadId, IEnumerable<string> chunkFiles, string chunkDirectory, string randomFileName);
+        Task<bool> MergeChunks(Guid uploadId, string chunkDirectory, int totalChunks);
+        Task FinalizeUpload(Guid uploadId, string chunkDirectory, string fileName);
         Task SaveChunkToDisk(SecureUploadChunk chunk, Guid uploadId);
 
         void RemoveUpload(Guid uploadId);
