160f3c96a6f2a4e8e6073babc71f61f45f28592b
radek00 <radekw00@proton.me>
2026-03-17 23:41:10 +0100

    button pointer and highlit issue fix

--- SecureSend/ClientApp/src/components/StyledButton.vue
+++ SecureSend/ClientApp/src/components/StyledButton.vue
@@ -2,7 +2,7 @@
   <button
     class="text-white focus:ring-4 focus:enabled::outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:cursor-not-allowed disabled:bg-gray-300 dark:disabled:bg-gray-700 disabled:text-gray-500"
     :class="{
-      'bg-blue-600 hover:enabled::bg-blue-700 focus:enabled:ring-blue-300 dark:focus:enabled:ring-blue-800':
+      'bg-blue-600 hover:enabled:bg-blue-700 focus:enabled:ring-blue-300 dark:focus:enabled:ring-blue-800':
         category === ButtonType.primary,
       'bg-gray-600 hover:enabled:bg-gray-700 focus:enabled:ring-gray-300 dark:bg-gray-800 dark:hover:enabled:bg-gray-900 dark:focus:enabled:ring-gray-700':
         category === ButtonType.back,
