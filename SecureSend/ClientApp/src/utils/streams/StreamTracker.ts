efabc36583932ada27768e876325285333b313aa
radek00 <radekw00@proton.me>
2026-03-22 13:55:42 +0100

    start chunk from 1

--- SecureSend/ClientApp/src/utils/streams/StreamTracker.ts
+++ SecureSend/ClientApp/src/utils/streams/StreamTracker.ts
@@ -1,11 +1,14 @@
 export default class StreamTracker {
   private readonly totalChunks: number;
-  private currentChunk = 0;
+  private currentChunk = 1;
   private readonly fileName: string;
   private readonly broadcast = new BroadcastChannel("progress-channel");
   constructor(totalSize: number, fileName: string) {
     this.fileName = fileName;
-    this.totalChunks = Math.ceil(totalSize / (5 * 1024 * 1024 + 16));
+    this.totalChunks = Math.max(
+      1,
+      Math.ceil(totalSize / (5 * 1024 * 1024 + 16))
+    );
   }
 
   public async transform(
@@ -21,6 +24,7 @@ export default class StreamTracker {
       this.currentChunk++;
       controller.enqueue(chunk);
     } catch (error) {
+      this.broadcast.close();
       controller.error(error);
     }
   }
