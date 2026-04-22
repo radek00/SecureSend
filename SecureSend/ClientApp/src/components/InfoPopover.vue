f2a78f48b165a3f679b72997adc513b1f49446ed
radek00 <radekw00@proton.me>
2026-04-22 18:23:29 +0200

    fix popover accesibility issues

--- SecureSend/ClientApp/src/components/InfoPopover.vue
+++ SecureSend/ClientApp/src/components/InfoPopover.vue
@@ -5,11 +5,12 @@
       class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-200 dark:focus:ring-gray-400 rounded-full"
       type="button"
     >
+      <span class="sr-only">Show information</span>
       <QuestionCircle class="w-5 h-5"></QuestionCircle>
     </button>
     <div
       v-if="isExpanded"
-      class="absolute p-3 text-sm font-light text-gray-500 bg-white border border-gray-200 rounded-lg shadow-sm dark:text-gray-400 dark:border-gray-600 dark:bg-gray-900 bottom-[65%] mb-2 w-64 left-1/2 transform -translate-x-1/2"
+      class="absolute p-3 text-sm font-light text-gray-600 bg-gray-50 border border-gray-200 rounded-lg shadow-sm dark:text-gray-400 dark:border-gray-600 dark:bg-gray-900 bottom-[65%] mb-2 w-64 left-1/2 transform -translate-x-1/2"
     >
       <slot></slot>
     </div>
