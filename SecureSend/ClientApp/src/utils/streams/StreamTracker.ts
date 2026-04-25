e81faed16bc2c0908b59ac299e18bbbcc7ccd01c
radek00 <radekw00@proton.me>
2026-04-25 14:26:41 +0200

    change how progress is being tracked

--- SecureSend/ClientApp/src/utils/streams/StreamTracker.ts
+++ SecureSend/ClientApp/src/utils/streams/StreamTracker.ts
@@ -1,14 +1,11 @@
 export default class StreamTracker {
-  private readonly totalChunks: number;
-  private currentChunk = 0;
+  private readonly totalBytes: number;
+  private processedBytes = 0;
   private readonly fileName: string;
   private readonly broadcast = new BroadcastChannel("progress-channel");
   constructor(totalSize: number, fileName: string) {
     this.fileName = fileName;
-    this.totalChunks = Math.max(
-      1,
-      Math.ceil(totalSize / (5 * 1024 * 1024 + 16))
-    );
+    this.totalBytes = Math.max(1, totalSize);
   }
 
   public async transform(
@@ -17,12 +14,12 @@ export default class StreamTracker {
   ) {
     try {
       controller.enqueue(chunk);
+      this.processedBytes += chunk.byteLength;
       this.broadcast.postMessage({
         request: "progress",
-        value: `${Math.ceil((this.currentChunk / this.totalChunks) * 100)}%`,
+        value: `${Math.min(Math.floor((this.processedBytes / this.totalBytes) * 100), 99)}%`,
         fileName: this.fileName,
       });
-      this.currentChunk++;
     } catch (error) {
       this.broadcast.close();
       controller.error(error);
